d7a7aa95f8a85a56b2a05d9843875605edc30b23
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-11-04 22:05:26 -0400

    Center popup on screen

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+// https://www.photopea.com/#%7B%22files%22%3A%5B%22https%3A%2F%2Fwww.photopea.com%2Fapi%2Fimg2%2Fpug.png%22%5D%2C%22environment%22%3A%7B%22plugins%22%3A%5B%7B%22name%22%3A%22pgf2%22%2C%22url%22%3A%22http%3A%2F%2Flocalhost%3A1995%2F%22%2C%22icon%22%3A%22https%3A%2F%2Fen.wikipedia.org%2Fstatic%2Ffavicon%2Fwikipedia.ico%22%7D%5D%7D%7D
+
 function handleFinalImage(data) {
     let b64uri = data[1];
     let img = new Image();
@@ -10,8 +12,19 @@ function createPopup(w, h, imgURI) {
     pluginURL.searchParams.set("popupPlugin", "yeah");
     pluginURL.searchParams.set("docWidth", w);
     pluginURL.searchParams.set("docHeight", h);
+
+    let popupOptions = {
+        width: 1400,
+        height: 700,
+        left: window.outerWidth / 2 - 700,
+        top: window.outerHeight / 2 - 350,
+    };
+    let windowFeatures = "";
+    for (let key in popupOptions) {
+        windowFeatures += key + "=" + popupOptions[key] + ",";
+    }
     
-    let popup = window.open(pluginURL, "_blank", "width=1400,height=700");
+    let popup = window.open(pluginURL, "_blank", windowFeatures);
     popup.window.addEventListener("message", function (e) {
         if (e.data[0] == "pluginStatus" && e.data[1] == "ready") {
             popup.window.postMessage(["refImage", imgURI]);
