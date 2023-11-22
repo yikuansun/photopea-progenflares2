c3e5f0af5621ce5e3cd6cbc8ec1b1177b2f0493b
Yikuan Sun <yikuansun2015@gmail.com>
2023-11-22 10:10:06 -0500

    Show message when popup loaded

--- code.js
+++ code.js
@@ -1,5 +1,7 @@
 // https://www.photopea.com/#%7B%22files%22%3A%5B%22https%3A%2F%2Fwww.photopea.com%2Fapi%2Fimg2%2Fpug.png%22%5D%2C%22environment%22%3A%7B%22plugins%22%3A%5B%7B%22name%22%3A%22pgf2%22%2C%22url%22%3A%22http%3A%2F%2Flocalhost%3A1995%2F%22%2C%22icon%22%3A%22http%3A%2F%2Flocalhost%3A1995%2Ficon120.png%22%7D%5D%7D%7D
 
+let $ = function(e) { return document.querySelector(e); };
+
 async function handleFinalImage(data) {
     let b64uri = data[1];
     await Photopea.runScript(window.parent, "app.activeDocument.activeLayer = app.activeDocument.layers[0];");
@@ -35,10 +37,19 @@ function createPopup(w, h, imgURI) {
             popup.window.close();
         }
     });
+
+    return popup;
 }
 
 getDocumentAsImage(window.parent).then(function(img) {
     img.addEventListener("load", function() {
-        createPopup(img.width, img.height, img.src);
+        let popup = createPopup(img.width, img.height, img.src);
+        $("#loadingSpinner").style.display = "none";
+        if (popup) {
+            $("#message").innerText = "Plugin opened in a popup window.";
+        }
+        else {
+            $("#message").innerText = "Plugin failed to open.\nPlease allow popups from Photopea.";
+        }
     });
 });
